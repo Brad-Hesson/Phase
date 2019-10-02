dd18421862839d0d88b20ccc236281877d1b0231
Brad Hesson <brad.hesson@outlook.com>
2019-10-01 20:21:07 -0600

    greatly improved file readers

--- src/nodes/file_balance.py
+++ src/nodes/file_balance.py
@@ -4,66 +4,67 @@ from _tkinter import TclError
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.widgets import Slider
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', '-f', default='data_1569887045.hdf5')
-rv = parser.parse_args()
-
-with h5py.File(rv.file, 'r') as f:
-    close_history = np.array(f['close'])
-    wide_history = np.array(f['wide'])
-
+filename = 'SN1_slow_ramp.hdf5'
 default_gain = 49.5
 g_rot = (0.977795681556585-0.20956050469803128j)
 
-close_history[:, 1] *= 1e6 / default_gain ** 2 / g_rot
-close_history[:, 0] -= close_history[0, 0]
-wide_history[:, 1] *= 1e6 / default_gain
-wide_history[:, 0] -= wide_history[0, 0]
 
-fig = plt.figure()
-ax_polar = fig.add_axes([0.125, 0.15, 0.35, 0.8])
-ax_polar.set_xlabel('Resistive (uV)')
-ax_polar.set_ylabel('Mechanical (uV)')
-ax_polar.grid()
-line_polar_close = ax_polar.plot([])[0]
-ax_polar.set_aspect(1)
-line_polar_wide = ax_polar.plot([])[0]
-radius = 6 / default_gain / default_gain * 1e6
-ax_polar.plot(radius * np.cos(np.linspace(0, 2*np.pi)), radius * np.sin(np.linspace(0, 2*np.pi)))
+def main(rv):
+    with h5py.File(rv.file, 'r') as f:
+        close_history = np.array(f['close'][::500])
+        wide_history = np.array(f['wide'][::500])
 
-slider = Slider(fig.add_axes([0.125, 0.1, 0.35, 0.03]), 'History', 1, max(len(close_history), len(wide_history)), 1000, valstep=1)
+    close_history[:, 1] *= 1e6 / default_gain ** 2 / g_rot
+    close_history[:, 0] -= close_history[0, 0]
+    wide_history[:, 1] *= 1e6 / default_gain
+    wide_history[:, 0] -= wide_history[0, 0]
 
-ax_real = fig.add_subplot(222, title='Resistive')
-ax_real.xaxis.set_visible(False)
-ax_real.set_ylabel('uV')
-line_real_close = ax_real.plot([])[0]
-line_real_wide = ax_real.plot([])[0]
+    index_ratio = len(wide_history) / wide_history[-1, 0].real * 60
 
-ax_imag = fig.add_subplot(224, sharex=ax_real, title='Mechanical')
-ax_imag.set_xlabel('Minutes')
-ax_imag.set_ylabel('uV')
-line_imag_close = ax_imag.plot([])[0]
-line_imag_wide = ax_imag.plot([])[0]
-ax_real.set_xlim(-5, 0)
-ax_real.set_ylim(-2500, 2500)
-ax_imag.set_ylim(-2500, 2500)
+    fig = plt.figure()
 
-fig.show()
+    ax_polar = fig.add_subplot(121)
+    ax_polar.set_xlabel('Resistive (uV)')
+    ax_polar.set_ylabel('Mechanical (uV)')
+    ax_polar.grid()
+    line_polar_close = ax_polar.plot([])[0]
+    ax_polar.set_aspect(1)
+    line_polar_wide = ax_polar.plot([])[0]
+    radius = 6 / default_gain / default_gain * 1e6
+    ax_polar.plot(radius * np.cos(np.linspace(0, 2*np.pi)), radius * np.sin(np.linspace(0, 2*np.pi)))
 
-run = True
-while run:
-    try:
-        history = int(slider.val)
-        line_polar_close.set_data(close_history[-history:, 1].real, close_history[-history:, 1].imag)
-        line_polar_wide.set_data(wide_history[-history:, 1].real, wide_history[-history:, 1].imag)
-        line_real_close.set_data(close_history[:, 0].real / 60., close_history[:, 1].real)
-        line_imag_close.set_data(close_history[:, 0].real / 60., close_history[:, 1].imag)
-        line_real_wide.set_data(wide_history[:, 0].real / 60., wide_history[:, 1].real)
-        line_imag_wide.set_data(wide_history[:, 0].real / 60., wide_history[:, 1].imag)
+    ax_real = fig.add_subplot(222, title='Resistive')
+    ax_real.xaxis.set_visible(False)
+    ax_real.set_ylabel('uV')
+    ax_real.plot(close_history[:, 0].real / 60., close_history[:, 1].real)
+    ax_real.plot(wide_history[:, 0].real / 60., wide_history[:, 1].real)
 
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-    except TclError:
-        run = False
+    ax_imag = fig.add_subplot(224, sharex=ax_real, title='Mechanical')
+    ax_imag.set_xlabel('Minutes')
+    ax_imag.set_ylabel('uV')
+    ax_imag.plot(close_history[:, 0].real / 60., close_history[:, 1].imag)
+    ax_imag.plot(wide_history[:, 0].real / 60., wide_history[:, 1].imag)
+
+    fig.show()
+
+    run = True
+    while run:
+        try:
+            start = max(0, int(ax_real.get_xlim()[0] * index_ratio))
+            end = int(ax_real.get_xlim()[1] * index_ratio)
+
+            line_polar_close.set_data(close_history[start:end, 1].real, close_history[start:end, 1].imag)
+            line_polar_wide.set_data(wide_history[start:end, 1].real, wide_history[start:end, 1].imag)
+
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        except TclError:
+            run = False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', '-f', default=filename)
+    rv = parser.parse_args()
+    main(rv)
--- src/nodes/file_temp.py
+++ src/nodes/file_temp.py
@@ -5,36 +5,60 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', '-f', default='SN1_slow_ramp.hdf5')
-rv = parser.parse_args()
+filename = 'SN1_slow_ramp.hdf5'
+start = 12000
+end = -181000
+step = 1000
 
-with h5py.File(rv.file, 'r') as f:
-    wide_history = np.array(f['wide'])
-    temp_history = np.array(f['temp'])
-    setpoint_history = np.array(f['setpoint'])
 
-default_gain = 49.5
-g_rot = (0.977795681556585-0.20956050469803128j)
+def main(rv):
+    with h5py.File(rv.file, 'r') as f:
+        wide_history = np.array(f['wide'][start:end:step])
+        temp_history = np.array(f['temp'][start:end:step])
+        setpoint_history = np.array(f['setpoint'][start:end:step])
 
-wide_history[:, 1] *= 1e6 / default_gain
-wide_history[:, 0] -= wide_history[0, 0]
-temp_history[:, 0] -= temp_history[0, 0]
-setpoint_history[:, 0] -= setpoint_history[0, 0]
+    default_gain = 49.5
+    g_rot = (0.977795681556585-0.20956050469803128j)
 
-fig = plt.figure()
-ax = fig.add_subplot(211)
-ax.plot(wide_history[:, 0].real / 60. / 60., np.abs(wide_history[:, 1] - wide_history[0, 1]))
-ax2 = fig.add_subplot(212, sharex=ax)
-ax2.plot(setpoint_history[:, 0] / 60. / 60., setpoint_history[:, 1])
-ax2.plot(temp_history[:, 0] / 60. / 60., temp_history[:, 1])
+    wide_history[:, 1] *= 1e6 / default_gain
+    wide_history[:, 0] -= wide_history[0, 0]
+    temp_history[:, 0] -= temp_history[0, 0]
+    setpoint_history[:, 0] -= setpoint_history[0, 0]
 
-fig.show()
+    fig = plt.figure()
 
-run = True
-while run:
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-    except TclError:
-        run = False
+    ax_b = fig.add_subplot(222, title='Balance vs. Time')
+    ax_b.xaxis.set_visible(False)
+    ax_b.set_ylabel('Imbalance (uV)')
+    ax_b.plot(wide_history[:, 0].real / 60. / 60., (wide_history[:, 1] - wide_history[0, 1]).real)
+    ax_b.plot(wide_history[:, 0].real / 60. / 60., (wide_history[:, 1] - wide_history[0, 1]).imag)
+
+    ax_t = fig.add_subplot(224, sharex=ax_b, title='Temperature vs. Time')
+    ax_t.set_ylabel('Temperature (C)')
+    ax_t.set_xlabel('Time (h)')
+    ax_t.plot(setpoint_history[:, 0] / 60. / 60., setpoint_history[:, 1], 'g-')
+    ax_t.plot(temp_history[:, 0] / 60. / 60., temp_history[:, 1], 'r-')
+
+    ax_bt = fig.add_subplot(121, title='Balance vs. Temperature')
+    ax_bt.plot(temp_history[:, 1], wide_history[:, 1].real - wide_history[0, 1].real)
+    ax_bt.plot(temp_history[:, 1], wide_history[:, 1].imag - wide_history[0, 1].imag)
+    ax_bt.annotate("    Start", (25, 0))
+    ax_bt.annotate("    End", (25, wide_history[-1, 1].real - wide_history[0, 1].real))
+    ax_bt.annotate("    End", (25, wide_history[-1, 1].imag - wide_history[0, 1].imag))
+
+    fig.show()
+
+    run = True
+    while run:
+        try:
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        except TclError:
+            run = False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', '-f', default=filename)
+    rv = parser.parse_args()
+    main(rv)
